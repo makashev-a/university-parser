609b86443c4486651264eb16ad25bff4dfd8b301
Akhmadi <ahmadi98ahmadi@gmail.com>
2021-08-19 19:32:39 +0600

    parser modified #2

--- app.py
+++ app.py
@@ -35,6 +35,24 @@ def get_content(html):
         title = item.find('div', class_='university__info--title').get_text(strip=True)
         link = item.find_all('div', class_='uni-contact__left--email')[0].find('a').get('href').strip()
         price = item.find_all('div', class_='institutions__info--price-block')[0].get_text(strip=True)
+
+        details = item.find('div', class_='section__lineinfo').find_all('div', class_='lineinfo__right')
+        details_list = []
+        d_list = []
+        for detail in details:
+            # print(detail.get_text(strip=True))
+            # detail_text = detail.find('div', class_='lineinfo__text').get_text(strip=True)
+            detail_number = detail.find('div', class_='lineinfo__number').get_text(strip=True)
+            # details_list.append(detail_text)
+            details_list.append(detail_number)
+        # for i in range(0, len(details_list), 2):
+        #     d_list.append(details_list[i] + ' - ' + details_list[i + 1])
+        # d_list = ", ".join(d_list)
+        student_number = details_list[0]
+        foreign_number = details_list[1]
+        teacher_number = details_list[2]
+        year_of_foundation = details_list[3]
+
         if re.search('[0-9]', price):
             price = price
             price = re.findall(r'\d+', price)
@@ -53,7 +71,11 @@ def get_content(html):
             'title': title,
             'link': link,
             'price': price,
-            'specialty': spec_lst
+            'specialty': spec_lst,
+            'student_number': student_number,
+            'foreign_number': foreign_number,
+            'teacher_number': teacher_number,
+            'year_of_foundation': year_of_foundation
         })
     return universities_list
 
@@ -62,9 +84,9 @@ def save_file(items, path):
     with open(path, 'w', newline='') as file:
         writer = csv.writer(file, delimiter=';')
         writer.writerow(
-            ['id', 'Наименование', 'Ссылка', 'Цена за год обучения', 'Кол-во специальностей'])
+            ['id', 'title', 'link', 'price', 'specialty', 'student_number', 'foreign_number', 'teacher_number', 'year_of_foundation'])
         for index, item in enumerate(items, start=1):
-            writer.writerow([index, item['title'], item['link'], item['price'], item['specialty']])
+            writer.writerow([index, item['title'], item['link'], item['price'], item['specialty'], item['student_number'], item['foreign_number'], item['teacher_number'], item['year_of_foundation']])
 
 
 universities = []
--- csv_to_postgres.py
+++ csv_to_postgres.py
@@ -1,23 +1,65 @@
 import psycopg2
+import sqlalchemy
+from sqlalchemy.exc import DBAPIError
 
 FILE = 'universities.csv'
-conn = psycopg2.connect("host= localhost dbname=aiogram user=akhmadi password=HaBuR394760")
+# conn = psycopg2.connect("host= localhost dbname=aiogram user=akhmadi password=HaBuR394760")
+engine = sqlalchemy.create_engine('postgresql://akhmadi:HaBuR394760@localhost/aiogram')
+conn = engine.raw_connection()
 print("Подключение к Базе данных")
-cur = conn.cursor()
+# cur = conn.cursor()
+try:
+    with conn.cursor() as cur:
+        cur.execute("""CREATE TABLE IF NOT EXISTS universities(
+        id integer PRIMARY KEY,
+        title varchar,
+        link varchar,
+        price varchar,
+        specialty integer,
+        student_number varchar,
+        foreign_number varchar,
+        teacher_number varchar,
+        year_of_foundation varchar
+        )
+         """)
 
-cur.execute("""CREATE TABLE IF NOT EXISTS universities(
-id integer PRIMARY KEY,
-title varchar,
-link varchar,
-price varchar,
-specialty integer
-)
- """)
+        cur.execute("""CREATE TEMPORARY TABLE universities_staging ( LIKE universities )
+        ON COMMIT DROP
+        """)
 
-with open('universities.csv', 'r') as f:
-    next(f)
-    cur.copy_from(f, 'universities', sep=';', columns=('id', 'title', 'link', 'price', 'specialty'))
-conn.commit()
-print("Данные были успешно перенесены")
-cur.close()
-conn.close()
+        with open(FILE, 'r') as data:
+            next(data)
+            cur.copy_from(data, 'universities_staging', sep=';', columns=('id', 'title', 'link', 'price', 'specialty', 'student_number', 'foreign_number', 'teacher_number', 'year_of_foundation'))
+
+        cur.execute("""INSERT INTO universities ( id, title, link, price, specialty, student_number, foreign_number, teacher_number, year_of_foundation)
+                               SELECT id, title, link, price, specialty, student_number, foreign_number, teacher_number, year_of_foundation
+                               FROM universities_staging
+                               ON CONFLICT ( id )
+                               DO UPDATE SET title = EXCLUDED.title
+                                           , link = EXCLUDED.link
+                                           , price = EXCLUDED.price
+                                           , specialty = EXCLUDED.specialty
+                                           , student_number = EXCLUDED.student_number
+                                           , foreign_number = EXCLUDED.foreign_number
+                                           , teacher_number = EXCLUDED.teacher_number
+                                           , year_of_foundation = EXCLUDED.year_of_foundation""")
+
+except:
+    conn.rollback()
+    raise
+
+else:
+    conn.commit()
+    print("Данные были успешно перенесены")
+
+finally:
+    conn.close()
+    print("Закрываем подключение")
+
+# with open(FILE, 'r') as f:
+# next(f)
+# cur.copy_from(f, 'universities', sep=';', columns=('id', 'title', 'link', 'price', 'specialty'))
+
+# conn.commit()
+# cur.close()
+# conn.close()
